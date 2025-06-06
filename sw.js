9bf99df352f8a507f1f1c26dd8748a13fdab3364
rpdNick <rpd_nick@ukr.net>
2025-06-06 19:39:42 +0300

    urlsToCache

--- sw.js
+++ sw.js
@@ -4,7 +4,7 @@ const CACHE_NAME = 'my-test-pwa-cache-v1';
 
 // Список URL-адрес, які потрібно кешувати під час встановлення Service Worker
 const urlsToCache = [
-  '/test_PWA',
+  '/test_PWA/',
   'index.html',
   './style/style.css',
   './js/script.js',
